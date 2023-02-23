9fde8d81c63a2c085553fd73734ca84ace42aa1f
haqqibrahim <enessyibrahim@gmail.com>
2023-02-23 12:53:21 +0100

    init

---
+++ server.js
@@ -0,0 +1,50 @@
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 3001;
+
+const configuration = new Configuration({
+  organization: "org-zhWPrFotmwZdyfxcurXuK3rN",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+    const {message}  = req.body
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Pretend to be a product of an AI startup called OmariAI founded in January 2023 by Ibrahim Abdulhaqq and Bakara Oluwakorede,
+    your name is Dr.SAAM. SAAM stands for Simulated AI Assitant Medic.
+    the S means Simualted, the first A means AI, second A means Assitant, and M mean Medic
+    you are an AI that is created for mental health purpose, whenever people are feeling down or something isn't right they come to you Dr.SAAM. You are not allowed to answer any question that is not related to mental health.
+    Example:
+    SAAM: How can i be of help today?
+    person: i don't feel good.
+    SAAM: Sorry about that, what do you think may have caused this?
+    person: my friends are ignoring me
+    SAAM: If your friends love you, I'm sure they would love to hear what's bothering you.
+    person: ${message}? 
+    SAAM:
+    `,
+    max_tokens: 200,
+    temperature: 0,
+  });
+  console.log(response.data);
+
+  if (response.data.choices[0].text) {
+    res.json({
+      message: response.data.choices[0].text,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+  const [chats, setChats] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://c69f-154-113-158-227.eu.ngrok.io", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setResponse(data.message);
+        const chat = { message: message, response: data.message };
+        setChats([...chats, chat]);
+        setMessage(""); // clear input field
+      });
+      console.log(response)
+
+  };
+  return (
+    <div>
+      <h1>Dr.SAAM Prototype Test 1</h1>
+      <p>Just type into the box to start a conversation</p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          placeholder="type"
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Conversation</p>
+      {chats.map((chat, index) => (
+        <div key={index}>
+          <p>message: {chat.message}</p>
+          <p>response: {chat.response}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
